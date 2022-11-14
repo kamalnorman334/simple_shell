fa941f6628be3cfd739e8176e58d3b902ae7abbb
kamalnorman334 <kamalnorman334@gmail.com>
2022-11-14 08:53:37 -0800

    main

--- shell.h
+++ shell.h
@@ -4,10 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <string.h>
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
+#include <limits.h>
 #include <unistd.h>
 #include <errno.h>
 
